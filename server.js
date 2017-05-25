649af6714c109cbcea0f9690259309d6ca526783
Karl74 <carlos.castro74@gmail.com>
2017-05-25 13:16:20 -0500

    scraping function

---
+++ server.js
@@ -0,0 +1,63 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Requiring our  models
+
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/economist");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+app.get("/scrape", function(req, res) {
+  request("https://www.google.com/finance", function(err, response, html) {
+    var $ = cheerio.load(html);
+    var result = [];
+    $(".title").each(function(i, element) {
+      var title = $(this).text();
+      var link = $(this).attr("href");
+      result.push({
+        title: title,
+        link: link
+      });
+    });
+    console.log(result);
+    res.json(result);
+  });
+}); //enf of /scrape
+
+
+
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
