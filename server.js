e66190ec9147165dde2c04dc2390e33a9993ef6f
Karl74 <carlos.castro74@gmail.com>
2017-05-27 23:09:56 -0500

    articles render in the page

--- server.js
+++ server.js
@@ -1,69 +1,74 @@
-// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-// Requiring our  models
+
 
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
-// Set mongoose to leverage built in JavaScript ES6 Promises
-mongoose.Promise = Promise;
 
-// Initialize Express
+
+var port = 3000;
+
 var app = express();
 
 // Use morgan and body parser with our app
 app.use(logger("dev"));
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
 
-// Make public a static dir
-// app.use(express.static("public"));
 
-// Database configuration with mongoose
-mongoose.connect("mongodb://localhost/scrapnews");
-var db = mongoose.connection;
+// |A|- SCRAP THE ARTICLES::::::::::::::::::::::::::::
 
-// Show any mongoose errors
-db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
+app.get("/scrape", function(req, res){
+
+	request("https://www.theatlantic.com/", function(error, response, html){
+
+		var $ = cheerio.load(html);
+		var result = [];
+		
+		$("p").each(function(i, element){
+
+			var title = $(this).text();
+		    var link = $(this).attr("href");
+		    result.push({
+			    title: title,
+			    link: link
+			  });
+		       	
+			console.log(result);
+		});  //end of method each
+			res.json(result);
+
+	}); //end of cheerio
+});//end of app.get
+
+// |B|- ALL ARTICLES VIEW ::::::::::::::::::::::::::::
+app.get("/articles", function(req, res){
+	res.render("articles");
 });
 
-// Once logged in to the db through mongoose, log a success message
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
-});
 
-app.get("/scrape", function(req, res) {
-  request("https://www.google.com/finance", function(err, response, html) {
-    var $ = cheerio.load(html);
-    var result = [];
-    $(".title").each(function(i, element) {
-      var title = $(this).text();
-      var link = $(this).attr("href");
-      result.push({
-        title: title,
-        link: link
-      });
-    });
-    console.log(result);
-    res.json(result);
-  });
-}); //enf of /scrape
-
-
-
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+// |END|- LISTENER FUNCTION :::::::::::::::::::::::::::
+app.listen(3000, function(){
+	console.log("App running on port 3000!")
 });
